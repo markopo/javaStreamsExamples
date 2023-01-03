bc1e85f12c22def0d04a1acab5138a4c11da1fdb
markopedu <69790644+markopedu@users.noreply.github.com>
2023-01-03 19:55:27 +0100

    sorted & grouped map

--- src/main/java/org/marko/Main.java
+++ src/main/java/org/marko/Main.java
@@ -1,18 +1,24 @@
 package org.marko;
 
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.*;
 
 public class Main {
     public static void main(String[] args) {
-//        System.out.println("Hello world!");
 
         var arr = new Integer[] { 5, 5, 2, 3, 2, 4, 10, 17, 13, 50, 5, 1, 2, 4 };
         var arrStream = Stream.of(arr);
-        var groupedArr = arrStream.collect(groupingBy(Integer::intValue));
+        var groupedMap = arrStream
+                            .collect(groupingBy(Integer::intValue));
 
-        groupedArr.forEach((val, list) -> System.out.println("Group: " + val + " num of: " + list.size()));
+        var groupedAndSortedArr = groupedMap.entrySet().stream().sorted(Map.Entry.comparingByKey())
+                .collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (a, b) -> b, LinkedHashMap::new));
+
+        groupedAndSortedArr.forEach((val, list) -> System.out.println("Group: " + val + " num of: " + list.size()));
 
 
 
